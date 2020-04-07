a2e346c85bcf578a601abed0e749c7a91c360699
Stephen Fang <safang99@gmail.com>
2020-04-07 14:16:31 -0400

    added index test for book_clubs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-book_club_one = BookClub.create(name: "ezLaunchers", location: "5th floor patio")
+book_club_one = BookClub.create(name: "Launchers", location: "deck 7")
 casi = Member.create(first_name: "Casi", last_name: "Newell", email: "casi@gmail.com", bio: "smarty smart smart" , favorite_book: "Something russian", leader: true, book_club: book_club_one )
 kerrin = Member.create(first_name: "Kerrin", last_name: "Gillis", email: "kerrin@gmail.com", bio: "styling guru", favorite_book: "Game of Thrones", leader: false, book_club: book_club_one)
 brianna = Member.create(first_name: "Brianna", last_name: "Kincart", email: "brianna@gmail.com", bio: "she's alright I guess", favorite_book: "The Alchemist", leader: false, book_club: book_club_one)
---
+++ spec/features/user_views_book_club_index_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+feature "book club index page" do
+  scenario "user views a list of all of the book clubs" do
+    
+  end
+end
