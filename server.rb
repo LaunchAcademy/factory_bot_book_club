8872057f994a4650d23be37983af36bba0a1f022
Nick Alberts <nwalberts5@gmail.com>
2022-07-18 17:49:47 -0400

    base

--- server.rb
+++ server.rb
@@ -6,23 +6,19 @@ set :bind, '0.0.0.0'  # bind to all interfaces
 enable :sessions
 
 get '/members' do
-  @members = Member.all
+  # ...
   
   erb :"members/index"
 end
 
 get '/book_clubs' do
-  @book_clubs = BookClub.all 
+  # ...
 
   erb :"book_clubs/index"
 end
 
 get '/book_clubs/:id' do
-  @book_club = BookClub.find(params[:id])
+  # ...
 
   erb :'book_clubs/show'
-end
-
-post "/book_clubs" do 
-
 end
